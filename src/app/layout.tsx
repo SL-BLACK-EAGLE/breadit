a772e36a9bf9582e8f4d5cfd21df357b416d4969
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-06-21 14:39:47 +0530

    make layout changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+import { cn } from '@/lib/utils';
 import '@/styles/globals.css'
+import {Inter} from "next/font/google"
 
 export const metadata = {
   title: 'Breadit',
   description: 'A Reddit clone built with Next.js and TypeScript.',
 }
+const inter = Inter({subsets: ['latin' ]});
+
 
 export default function RootLayout({
   children,
@@ -11,7 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={cn
+    ('bg-white text-slate-900 antialiased light', inter.className)}>
       <body>{children}</body>
     </html>
   )
